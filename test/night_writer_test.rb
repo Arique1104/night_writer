0f2c291bac73a76c78b44a10653c17eaf41aa381
Arique Aguilar <arique1104@gmail.com>
2020-07-31 21:57:10 -0500

    Adds required files

---
+++ test/night_writer_test.rb
@@ -0,0 +1,15 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "mocha/minitest"
+require "./lib/night_writer"
+
+class NightWriterTest < Minitest::Test
+
+  #def test_it_exists
+    #assert_instance_of <Class>,
+  #end
+
+  #def test_it_has_attributes
+  #end
+
+end
