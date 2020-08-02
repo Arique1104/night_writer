66b51dd1c0be47d2065f87da33391f5a10e8f6d2
Arique Aguilar <arique1104@gmail.com>
2020-08-02 13:18:58 -0500

    Test for set_braille_dictionary

--- test/braille_test.rb
+++ test/braille_test.rb
@@ -20,4 +20,18 @@ class BrailleTest < Minitest::Test
     assert_equal expected, @braille.dictionary
   end
 
+  def test_it_can_create_braille_initial_hash
+    # skip
+    #changing to actual braille dictionary
+
+  @braille.set_braille_hash
+
+  assert_equal "00..00", @braille.dictionary["x"]
+
+  assert_equal ".000..", @braille.dictionary["j"]
+
+  assert_equal "00....", @braille.dictionary["c"]
+
+  end
+
 end
