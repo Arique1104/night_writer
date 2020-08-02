28eef0236201b61a7b1b93a9c4716b955675e3f3
Arique Aguilar <arique1104@gmail.com>
2020-08-02 12:59:55 -0500

    Add set_braille_hash_method

--- test/invisible_ink_test.rb
+++ test/invisible_ink_test.rb
@@ -41,37 +41,14 @@ class InvisibleInkTest < Minitest::Test
     # skip
     #changing to actual braille dictionary
 
-  expected1 = {
-    "a" => ["0.","..", ".."],
-    "b" => ["0.","0.", ".."],
-    "c" => ["00","..", ".."],
-    "d" => ["00",".0", ".."],
-    "e" => ["0.",".0", ".."],
-    "f" => ["00","0.", ".."],
-    "g" => ["00","00", ".."],
-    "h" => ["0.","00", ".."],
-    "i" => [".0","0.", ".."],
-    "j" => [".0","00", ".."],
-    "k" => ["0.","..", "0."],
-    "l" => ["0.","0.", "0."],
-    "m" => ["00","..", "0."],
-    "n" => ["00",".0", "0."],
-    "o" => ["0.",".0", "0."],
-    "p" => ["00","0.", "0."],
-    "q" => ["00","00", "0."],
-    "r" => ["0.","00", "0."],
-    "s" => [".0","0.", "0."],
-    "t" => [".0","00", "0."],
-    "u" => ["0.","..", "00"],
-    "v" => ["0.","0.", "00"],
-    "w" => [".0","00", ".0"],
-    "x" => ["00","..", "00"],
-    "y" => ["00",".0", "00"],
-    "z" => ["0.",".0", "00"],
-    }
   @invisible_ink.set_braille_hash
 
-  assert_equal expected1, @invisible_ink.braille_dictionary
+  assert_equal "00..00", @invisible_ink.braille_dictionary["x"]
+
+  assert_equal ".000..", @invisible_ink.braille_dictionary["j"]
+
+  assert_equal "00....", @invisible_ink.braille_dictionary["c"]
+
   end
 
 
