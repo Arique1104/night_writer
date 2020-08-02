2e6c840dc67cc4cbc532ee93a0525fa29c69999c
Arique Aguilar <arique1104@gmail.com>
2020-08-02 13:25:06 -0500

    Adds set_braille_dictionary to braille class

--- lib/braille.rb
+++ lib/braille.rb
@@ -4,4 +4,33 @@ class Braille
     @dictionary = {}
   end
 
+  def set_braille_dictionary
+    @dictionary["a"] = "0....."
+    @dictionary["b"] = "0.0..."
+    @dictionary["c"] = "00...."
+    @dictionary["d"] = "00.0.."
+    @dictionary["e"] = "0..0.."
+    @dictionary["f"] = "000..."
+    @dictionary["g"] = "0000.."
+    @dictionary["h"] = "0.00.."
+    @dictionary["i"] = ".00..."
+    @dictionary["j"] = ".000.."
+    @dictionary["k"] = "0...0."
+    @dictionary["l"] = "0.0.0."
+    @dictionary["m"] = "00..0."
+    @dictionary["n"] = "00.00."
+    @dictionary["o"] = "0..00."
+    @dictionary["p"] = "000.0."
+    @dictionary["q"] = "00000."
+    @dictionary["r"] = "0.000."
+    @dictionary["s"] = ".00.0."
+    @dictionary["t"] = ".0000."
+    @dictionary["u"] = "0...00"
+    @dictionary["v"] = "0.0.00"
+    @dictionary["w"] = ".000.0"
+    @dictionary["x"] = "00..00"
+    @dictionary["y"] = "00.000"
+    @dictionary["z"] = "0..000"
+  end
+
 end
