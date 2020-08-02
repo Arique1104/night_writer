aed77e0147f2f924159e34969c52ab0f76f8b3f7
Arique Aguilar <arique1104@gmail.com>
2020-08-02 10:16:35 -0500

    Test it can write in braille

--- test/invisible_ink_test.rb
+++ test/invisible_ink_test.rb
@@ -21,13 +21,21 @@ class InvisibleInkTest < Minitest::Test
   end
 
   def test_it_can_read_file
-    assert_equal "Hello World!\n", @invisible_ink.read_file
+    assert_equal "Hi", @invisible_ink.read_file
 
   end
 
   def test_it_can_create_a_new_file
     ###this will need a mocks and stub
-    assert_equal 13, @invisible_ink.write_file
+    expected = "Hi"
+    assert_equal 3, @invisible_ink.write_file
+    assert_equal expected, File.read(@invisible_ink.new_file)
+  end
+
+  def test_test_it_can_write_in_braille
+    expected = [["0.","00", ".."],[".0","0.", ".."]]
+    assert_equal 3, @invisible_ink.write_file
+    assert_equal expected, File.read(@invisible_ink.new_file.complete_braille)
   end
 
 # THIS TEST NEEDS A MOCKS AND STUB
@@ -45,32 +53,32 @@ class InvisibleInkTest < Minitest::Test
     assert_equal expected1, @invisible_ink.braille_dictionary
 
     expected2 = {
-          "a" => "0.\n..\n..",
-          "b" => "0.\n0.\n..",
-          "c" => "00\n..\n..",
-          "d" => "00\n.0\n..",
-          "e" => "0.\n.0\n..",
-          "f" => "00\n0.\n..",
-          "g" => "00\n00\n..",
-          "h" => "0.\n00\n..",
-          "i" => ".0\n0.\n..",
-          "j" => ".0\n00\n..",
-          "k" => "0.\n..\n0.",
-          "l" => "0.\n0.\n0.",
-          "m" => "00\n..\n0.",
-          "n" => "00\n.0\n0.",
-          "o" => "0.\n.0\n0.",
-          "p" => "00\n0.\n0.",
-          "q" => "00\n00\n0.",
-          "r" => "0.\n00\n0.",
-          "s" => ".0\n0.\n0.",
-          "t" => ".0\n00\n0.",
-          "u" => "0.\n..\n00",
-          "v" => "0.\n0.\n00",
-          "w" => ".0\n00\n.0",
-          "x" => "00\n..\n00",
-          "y" => "00\n.0\n00",
-          "z" => "0.\n.0\n00",
+          "a" => ["0.","..", ".."],
+          "b" => ["0.","0.", ".."],
+          "c" => ["00","..", ".."],
+          "d" => ["00",".0", ".."],
+          "e" => ["0.",".0", ".."],
+          "f" => ["00","0.", ".."],
+          "g" => ["00","00", ".."],
+          "h" => ["0.","00", ".."],
+          "i" => [".0","0.", ".."],
+          "j" => [".0","00", ".."],
+          "k" => ["0.","..", "0."],
+          "l" => ["0.","0.", "0."],
+          "m" => ["00","..", "0."],
+          "n" => ["00",".0", "0."],
+          "o" => ["0.",".0", "0."],
+          "p" => ["00","0.", "0."],
+          "q" => ["00","00", "0."],
+          "r" => ["0.","00", "0."],
+          "s" => [".0","0.", "0."],
+          "t" => [".0","00", "0."],
+          "u" => ["0.","..", "00"],
+          "v" => ["0.","0.", "00"],
+          "w" => [".0","00", ".0"],
+          "x" => ["00","..", "00"],
+          "y" => ["00",".0", "00"],
+          "z" => ["0.",".0", "00"],
       }
       @invisible_ink.set_hash
     assert_equal expected2, @invisible_ink.braille_dictionary
@@ -79,17 +87,18 @@ class InvisibleInkTest < Minitest::Test
 
   def test_it_can_find_a_braille_equivalent_for_each_letter
     @invisible_ink.set_hash
-    assert_equal "0.\n..\n..", @invisible_ink.find_braille("a")
+    assert_equal ["0.","..", ".."], @invisible_ink.find_braille("a")
 
   end
 
   def test_it_can_find_letter
     @invisible_ink.set_hash
-    assert_equal "a", @invisible_ink.find_letter("0.\n..\n..")
+    assert_equal "a", @invisible_ink.find_letter(["0.","..", ".."])
 
   end
 
   def test_it_can_create_braille_grid
+    skip
     #40 characters across means only 20 characters
 
     expected = [] #Letters for
