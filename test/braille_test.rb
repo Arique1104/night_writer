d1c454af8de1742b4f71ddf45e4f3ebf10464215
Arique Aguilar <arique1104@gmail.com>
2020-08-02 13:04:59 -0500

    Test it exists for braille class

--- test/braille_test.rb
+++ test/braille_test.rb
@@ -2,12 +2,18 @@ require "minitest/autorun"
 require "minitest/pride"
 require "mocha/minitest"
 require "./lib/braille"
+require "./lib/night_writer"
+require "./lib/invisible_ink"
 
 class BrailleTest < Minitest::Test
+  def setup
+    @braille = Braille.new
 
-  #def test_it_exists
-    #assert_instance_of <Class>,
-  #end
+  end
+  def test_it_exists
+    assert_instance_of Braille,
+    @braille
+  end
 
   #def test_it_has_attributes
   #end
