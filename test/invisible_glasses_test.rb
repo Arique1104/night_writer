741db2a1becfb6e54d1a90df7445b10b68e9619d
Arique Aguilar <arique1104@gmail.com>
2020-08-03 19:39:28 -0500

    Test it exists

--- test/invisible_glasses_test.rb
+++ test/invisible_glasses_test.rb
@@ -4,10 +4,15 @@ require "mocha/minitest"
 require "./lib/invisible_glasses"
 
 class InvisibleGlassesTest < Minitest::Test
-
-  #def test_it_exists
-    #assert_instance_of <Class>,
-  #end
+  def setup
+    @braille_file = "braille.txt"
+    @new_file = "original_message.txt"
+    @invisible_glasses = InvisibleGlasses.new(@braille_file, @new_file)
+  end
+  
+  def test_it_exists
+    assert_instance_of InvisibleGlasses, @invisible_glasses
+  end
 
   #def test_it_has_attributes
   #end
