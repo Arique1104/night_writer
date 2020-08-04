25f9e7ac017e77dfc6e1102ec22a53a482203cf7
Arique Aguilar <arique1104@gmail.com>
2020-08-04 09:10:17 -0500

    Test coverage ignored successful, addding test helper to invisible glasses

--- test/invisible_glasses_test.rb
+++ test/invisible_glasses_test.rb
@@ -1,7 +1,4 @@
-require "minitest/autorun"
-require "minitest/pride"
-require "mocha/minitest"
-require "./lib/invisible_glasses"
+require "./test/test_helper"
 
 class InvisibleGlassesTest < Minitest::Test
   def setup
