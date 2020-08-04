39b69b276661a791854637ebd59a86a3967e430d
Arique Aguilar <arique1104@gmail.com>
2020-08-03 20:31:53 -0500

    Adds set_english_dicttionary

--- lib/letter.rb
+++ lib/letter.rb
@@ -1,7 +1,46 @@
 class Letter
- attr_reader  :message
+ attr_reader  :message,
+              :english_dictionary
+
   def initialize(message)
     @message = message
+    @english_dictionary = {}
+  end
+
+  def set_english_dictionary
+    @english_dictionary["0....."] = "a"
+    @english_dictionary["0.0..."] = "b"
+    @english_dictionary["00...."] = "c"
+    @english_dictionary["00.0.."] = "d"
+    @english_dictionary["0..0.."] = "e"
+    @english_dictionary["000..."] = "f"
+    @english_dictionary["0000.."] = "g"
+    @english_dictionary["0.00.."] = "h"
+    @english_dictionary[".00..."] = "i"
+    @english_dictionary[".000.."] = "j"
+    @english_dictionary["0...0."] = "k"
+    @english_dictionary["0.0.0."] = "l"
+    @english_dictionary["00..0."] = "m"
+    @english_dictionary["00.00."] = "n"
+    @english_dictionary["0..00."] = "o"
+    @english_dictionary["000.0."] = "p"
+    @english_dictionary["00000."] = "q"
+    @english_dictionary["0.000."] = "r"
+    @english_dictionary[".00.0."] = "s"
+    @english_dictionary[".0000."] = "t"
+    @english_dictionary["0...00"] = "u"
+    @english_dictionary["0.0.00"] = "v"
+    @english_dictionary[".000.0"] = "w"
+    @english_dictionary["00..00"] = "x"
+    @english_dictionary["00.000"] = "y"
+    @english_dictionary["0..000"] = "z"
+    @english_dictionary["......"] = " "
+    @english_dictionary["..000."] = "!"
+    @english_dictionary["....0."] = "'"
+    @english_dictionary["..0..."] = ","
+    @english_dictionary["....00"] = "-"
+    @english_dictionary["..00.0"] = "."
+    @english_dictionary["..0.00"] = "?"
   end
 
 end
--- test/letter_test.rb
+++ test/letter_test.rb
@@ -2,6 +2,8 @@ require "minitest/autorun"
 require "minitest/pride"
 require "mocha/minitest"
 require "./lib/letter"
+require "./lib/braille"
+
 
 class LetterTest < Minitest::Test
 
@@ -16,6 +18,13 @@ class LetterTest < Minitest::Test
 
   def test_it_has_attributes
     assert_equal @message, @letter.message
+    expected = {}
+    assert_equal expected, @letter.english_dictionary
+  end
+
+  def test_it_can_set_english_dictionary
+     assert_equal "?", @letter.set_english_dictionary
+
   end
 
 end
